2c97cc394ef9c6a7c0316375f02d4169d949c99b
Jia Qing <jiaqing2303@gmail.com>
2021-01-06 21:51:29 +0800

    update

---
+++ PrivateTable/utils.py
@@ -0,0 +1,15 @@
+"""
+A collection of useful functions.
+"""
+import math
+from typing import Iterable, List
+
+import pandas as pd
+
+
+def check_positive(x: float):
+    assert x > 0, "expected a positive value."
+
+
+def check_absolute_error(x: float, y: float, error: float):
+    assert math.fabs(x-y) <= error
